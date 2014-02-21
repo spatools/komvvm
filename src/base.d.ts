c4de87b6a229d566c858cb0924e4f035559ba81d
SomaticIT <contact@somatic.fr>
2014-02-21 03:25:40 +0100

    Add operation module

---
+++ src/base.d.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="../typings/knockout/knockout.d.ts" />
+/// <reference path="../typings/underscore/underscore.d.ts" />
+
+interface OperationOptions {
+    useArguments?: boolean;
+    cache?: boolean;
+    cacheDuration?: number;
+    message?: string;
+
+    execute: () => any;
+    complete: () => any;
+    error: () => any;
+    progress: () => any;
+}
+
+interface OperationFunction extends Function {
+    isExecuting: KnockoutObservable<boolean>;
+    progress: KnockoutObservable<number>;
+    progressDetails: KnockoutObservable<any>;
+    error: KnockoutObservable<string>;
+    errorDetails: KnockoutObservable<any>;
+    hasError: KnockoutObservable<boolean>;
+}
+
+interface KnockoutBindingHandlers {
+    loader: KnockoutBindingHandler;
+}
